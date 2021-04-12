91382e651974d3ba39f71c34a8b3b7328408c381
Evan Siegel <siegel.evan@gmail.com>
2021-04-11 22:09:18 -0400

    Protects WineSection Initializers with isNonEmpty

--- assets/scripts/application.ts
+++ assets/scripts/application.ts
@@ -1,5 +1,6 @@
 import { map } from "./lib/Array";
 import { selectAll } from "./lib/DOM/Document";
+import { isNonEmpty } from "./lib/NonEmptyArray";
 import { WineSection, fromElement } from "./WineSection";
 import * as BackgroundColorChanger from "./BackgroundColorChanger";
 import * as WineCarousel from "./WineCarousel";
@@ -9,9 +10,11 @@ const wineEls = selectAll<HTMLElement>("section.Wine")(window.document);
 
 const wineSections: Array<WineSection> = map(fromElement)(wineEls);
 
-BackgroundColorChanger.initialize(wineSections);
+if (isNonEmpty(wineSections)) {
+  BackgroundColorChanger.initialize(wineSections);
 
-map(WineCarousel.initialize)(wineSections);
+  map(WineCarousel.initialize)(wineSections);
+}
 
 const imageHoverSwapEls = selectAll<HTMLImageElement>("img[data-hover-swap]")(
   window.document,
--- assets/scripts/lib/NonEmptyArray.ts
+++ assets/scripts/lib/NonEmptyArray.ts
@@ -2,12 +2,13 @@ import { failure, Result, success } from "./Result";
 
 export type NonEmptyArray<T> = Array<T> & { _: "NonEmptyArray" };
 
+export const isNonEmpty = <A>(arr: Array<A>): arr is NonEmptyArray<A> =>
+  arr.length !== 0;
+
 export const fromArray = <A>(
   arr: Array<A>,
 ): Result<string, NonEmptyArray<A>> => {
-  if (arr.length === 0) {
-    return failure("Cannot construct non-empty array from empty array.");
-  }
+  if (isNonEmpty(arr)) return success(arr);
 
-  return success(arr as NonEmptyArray<A>);
+  return failure("Cannot construct non-empty array from empty array.");
 };
