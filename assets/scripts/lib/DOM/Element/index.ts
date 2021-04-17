ca54ece1f14379a0718802eb565ba759df71ba0d
Evan Siegel <siegel.evan@gmail.com>
2021-04-17 10:01:27 -0400

    Adds DOM.Element.attr

--- assets/scripts/lib/DOM/Element/index.ts
+++ assets/scripts/lib/DOM/Element/index.ts
@@ -9,4 +9,7 @@ export const selectAll = <E extends Element = Element>(selector: string) => (
   el: Element,
 ): Array<E> => Array.from(el.querySelectorAll<E>(selector));
 
+export const attr = (name: string) => (el: Element): Maybe<string> =>
+  maybe(el.getAttribute(name));
+
 export { swipeObservable };
