e41d792f10cda49721e692336e7b957b2f911af8
Evan Siegel <siegel.evan@gmail.com>
2021-05-29 09:01:48 -0400

    Reformats lib/DOM/Document.ts

--- assets/scripts/lib/DOM/Document.ts
+++ assets/scripts/lib/DOM/Document.ts
@@ -1,9 +1,11 @@
 import { Maybe, maybe } from "../Maybe";
 
-export const select = <E extends Element = Element>(selector: string) => (
-  doc: Document,
-): Maybe<E> => maybe(doc.querySelector<E>(selector));
+export const select =
+  <E extends Element = Element>(selector: string) =>
+  (doc: Document): Maybe<E> =>
+    maybe(doc.querySelector<E>(selector));
 
-export const selectAll = <E extends Element = Element>(selector: string) => (
-  doc: Document,
-): Array<E> => Array.from(doc.querySelectorAll<E>(selector));
+export const selectAll =
+  <E extends Element = Element>(selector: string) =>
+  (doc: Document): Array<E> =>
+    Array.from(doc.querySelectorAll<E>(selector));
