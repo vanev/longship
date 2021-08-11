b25911527b4f25883f5d86676eeff851955bda58
Evan Siegel <siegel.evan@gmail.com>
2021-08-10 21:24:24 -0400

    Adds Active State for Location

--- assets/scripts/LocateSection/Location.ts
+++ assets/scripts/LocateSection/Location.ts
@@ -23,40 +23,46 @@ const coordsFromElement = (element: HTMLElement): Result<string, Coordinates> =>
     getCoordinate("longitude")(element),
   ]);
 
-export const fromElement = ({ map, pinSrc }: Map) => (
-  element: HTMLElement,
-): Result<string, Location> => {
-  const coords = coordsFromElement(element);
+export const fromElement =
+  ({ map, pinSrc }: Map) =>
+  (element: HTMLElement): Result<string, Location> => {
+    const coords = coordsFromElement(element);
 
-  if (isFailure(coords)) return coords;
+    if (isFailure(coords)) return coords;
 
-  const [lat, lng] = coords.value;
+    const [lat, lng] = coords.value;
 
-  const marker = new google.maps.Marker({
-    position: {
+    const marker = new google.maps.Marker({
+      position: {
+        lat,
+        lng,
+      },
+    });
+
+    if (pinSrc.tag === "Just") {
+      marker.setIcon({
+        url: pinSrc.value,
+        scaledSize: new google.maps.Size(45, 45),
+      });
+    }
+
+    marker.setMap(map);
+
+    element.addEventListener("click", () => {
+      element.parentElement
+        ?.querySelector("._active")
+        ?.classList.remove("_active");
+
+      element.classList.add("_active");
+
+      map.setCenter({ lat, lng });
+      map.setZoom(14);
+    });
+
+    return success({
+      element,
       lat,
       lng,
-    },
-  });
-
-  if (pinSrc.tag === "Just") {
-    marker.setIcon({
-      url: pinSrc.value,
-      scaledSize: new google.maps.Size(45, 45),
+      marker,
     });
-  }
-
-  marker.setMap(map);
-
-  element.addEventListener("click", () => {
-    map.setCenter({ lat, lng });
-    map.setZoom(14);
-  });
-
-  return success({
-    element,
-    lat,
-    lng,
-    marker,
-  });
-};
+  };
